91f711b1b120aef860e4aa83f2cdb40788561824
RH-MikkelWerling <mikkel.werling@regionh.dk>
2025-11-15 13:15:04 +0100

    rename and make readme better

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -1,4 +1,4 @@
-from lyfo_treatment_failure_prediction.data_processing.preprocess import main
+from lyfo_treatment_failure_prediction.data_processing.preprocess_data import main
 
 if __name__ == "__main__":
     main()
